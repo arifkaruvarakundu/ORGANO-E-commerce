e9c1b0dc870f4af5057eb55264a516826ffa6196
muhammed harif <harifn786@gmail.com>
2023-08-14 17:59:45 +0530

    wsgi problem solved

--- organo/organo/wsgi.py
+++ organo/organo/wsgi.py
@@ -7,19 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-<<<<<<< HEAD
-# organo/wsgi.py
-
-=======
->>>>>>> 01a0301d8dfcd546fea09e3e44adb69ca2c3a6d0
 import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'organo.settings')
 
-<<<<<<< HEAD
 application = get_wsgi_application()
-=======
-application = get_wsgi_application()
->>>>>>> 01a0301d8dfcd546fea09e3e44adb69ca2c3a6d0
+
